598cd769d03a8c7c7aa25e80161001166912dcc9
Raj Singh <raj.singh1+e2enetworks.com>
2026-02-10 11:05:20 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health():
+    return {"status": "Branched changed"}
+
+@app.route("/infer", methods=["POST"])
+def infer():
+    data = request.get_json(force=True)
+
+    # Dummy inference logic (replace later)
+    text = data.get("text", "")
+    result = text.upper()
+
+    return jsonify({
+        "input": text,
+        "output": result
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
